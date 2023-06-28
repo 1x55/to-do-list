9e7322dfb5892639a1fc011c13470fc10781b9b7
1x55 <k4i@pm.me>
2023-06-28 14:13:31 +0900

    added env file"
    ""
    "
    clear
    git status

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+//declare varibles
 const express = require("express");
 const app = express()
 const PORT = 8500;
 const mongoose = require("mongoose")
+require('dotenv').config();
+
+//set middleware
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect()
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
